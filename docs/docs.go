47cbff1615933f6dada5b68c1dea76e33ffdd4ea
heebin <heebin47@gmail.com>
2023-12-24 17:37:57 +0900

    DOCS modify swagger

--- docs/docs.go
+++ docs/docs.go
@@ -38,7 +38,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "0.0.1",
-	Host:             "localhost:8080",
+	Host:             "",
 	BasePath:         "/",
 	Schemes:          []string{},
 	Title:            "side-hub API",
